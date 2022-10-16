02e231c178023c5f4c097c0db6c52fa24ebf6310
namrata77 <namratathakur077@gmail.com>
2022-10-16 22:59:46 +0530

    Added css to home page

--- myApp.js
+++ myApp.js
@@ -5,6 +5,8 @@ const middleware = require("./middleware")
 const path = require('path')
 const bodyParser = require("body-parser")
 const mongoose = require("./database");
+const session  = require("express-session");
+
 
 
 const server = app.listen(port, () => 
@@ -17,16 +19,28 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(session({
+    secret : "bbq chips",
+    resave : true,
+    saveUninitialized : false
+
+}))
+
 //routes
 const loginRoute = require('./routes/loginRoutes');
 const registerRoute = require('./routes/registerRoutes');
+const logoutRoutes = require('./routes/logout');
+
+
 app.use("/login", loginRoute);
 app.use("/register", registerRoute);
+app.use("/logout", logoutRoutes);
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
 
     var payload = {
-        pageTitle: "Home"
+        pageTitle: "Home",
+        userLoggedIn: req.session.user
     }
     res.status(200).render("home", payload);
 })
--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,12 +1,51 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+const bodyParser = require('body-parser');
+const bcrypt = require("bcrypt");
+const User = require('../schemas/UserSchema');
+
 
 
 app.set("view engine", "pug");
 app.set("views", "views");
+app.use(bodyParser.urlencoded({extended: false}));
+
 
 router.get("/", (req, res, next) => {
+    res.status(200).render("login");
+})
+
+router.post("/", async (req, res, next) => {
+
+    var payload = req.body;
+
+    if(req.body.logUsername && req.body.logPassword){
+        var user = await User.findOne({
+            $or: [
+                { username: req.body.logUsername }, 
+                { email: req.body.logUsername }
+            ]
+        })
+        .catch((error) => {
+            console.log(error);
+            playload.errorMessage = "Something went wrong.";
+            res.status(200).render("login", payload);
+        });
+
+        if(user != null){
+            var result = await bcrypt.compare(req.body.logPassword, user.password);
+
+            if(result === true){
+                req.session.user = user;
+                return res.redirect("/");
+            } 
+            
+        }
+        payload.errorMessage = "Login Credentials Incorrect.";
+        res.status(200).render("login", payload); 
+    }
+    payload.errorMessage = "Make sure that each field has valid value!";
     res.status(200).render("login");
 })
 
